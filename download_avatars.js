3bd6b9756a779e65c66918b123537627c247b545
Hans Kurniadi <hans.d.k@live.com>
2017-02-14 02:20:44 +0000

    add github username and toke and request url

--- download_avatars.js
+++ download_avatars.js
@@ -2,6 +2,14 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = 'hkurniadi';
+var GITHUB_TOKEN = '9977f2941d4680fa042001d1e922e0e0d4162c99';
+var repoOwner = 'jquery';
+var repoName = 'jquery';
+
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL);
+
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
 }
